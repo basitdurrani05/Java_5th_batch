7095d2ac3cd3ab68cd7551b9d1801e09d27b0839
basitdurrani <basitdurrani05786@gmail.com>
2025-06-02 20:35:12 +0530

    Updated

---
+++ thirteen.java
@@ -0,0 +1,57 @@
+// Methods - It is a Resuable bit of code
+
+// Two Types
+
+// 1).Static 
+// 2). Non-Static
+
+public class thirteen {
+
+
+    // 1). Static
+
+    // public static void kriti(){
+
+    //     for(int i=0; i<=100; i++){
+
+    //         System.out.println("Hello World");
+    //     }
+
+// }
+
+    // 2). Non Static
+
+
+    public void basit(){
+
+        System.out.println("Basit Durrani");
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // Static Call
+
+    //    kriti();
+
+
+
+    // Non Static Call
+
+   
+    // thirteen kriti = new thirteen();
+
+    // kriti.basit();
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
