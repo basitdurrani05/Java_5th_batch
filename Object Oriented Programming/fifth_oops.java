6e34946277625942d1d91b9025e24b60982cf199
basitdurrani <basitdurrani05786@gmail.com>
2025-06-08 20:46:03 +0530

    Updated

---
+++ Object Oriented Programming/fifth_oops.java
@@ -0,0 +1,19 @@
+// 3). Private Constructor.
+
+public class fifth_oops {
+
+     private fifth_oops(){
+
+        System.out.println("I am Private Constructor....");
+    }
+
+    public static void main(String[] args) {
+
+        fifth_oops obj = new fifth_oops();
+
+
+
+        
+    }
+    
+}
---
+++ Object Oriented Programming/fourth_oops.java
@@ -0,0 +1,84 @@
+// Constructor : Special type of method that initialize the object.
+
+//  Type of Constructor.
+
+
+// 1).Paratermized
+// 2). Copy
+// 3). Private
+
+
+public class fourth_oops {
+
+    // Constructor
+
+    // fourth_oops(){
+
+    //     System.out.println("Hello I am Constructor...");
+    // }
+
+
+    // Type Of Constructor.
+
+
+    // 1). Paratermized.
+
+    // fourth_oops(int a,String b){
+
+    //     System.out.println("Roll Number : "+a);
+    //     System.out.println("Name : "+b);
+    // }
+
+
+    // 2). Copy Constructor.
+
+    // String a;
+    // String b;
+
+    // fourth_oops(){   // 1st Constructor
+
+    //     a = "My name is Basit Durrrani";
+    //     b = "My Roll Number Is 0904CS2112030";
+    //     System.out.println(a);
+    //     System.out.println(b);
+
+    // }
+
+    // fourth_oops(fourth_oops kriti){  // Copy Constructor
+
+
+    //     a = kriti.a;
+    //     b = kriti.b;
+    //     System.out.println(a);
+    //     System.out.println(b);
+    // }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        // Constructor
+        // fourth_oops obj = new fourth_oops();
+
+
+           // 1). Paratermized.
+
+        //    fourth_oops obj = new fourth_oops(30, "Basit");
+
+        // 2). Copy Constructor
+
+
+        // fourth_oops obj = new fourth_oops();
+
+        // fourth_oops obj2 = new fourth_oops(obj);
+
+
+
+        
+    }
+    
+}
